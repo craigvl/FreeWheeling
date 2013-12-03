dcd2fedca68a4a86409ee52af6088111aba1621e
Craig <craigvl@tpg.com.au>
2013-12-03 17:14:43 +1000

    Updates.

---
+++ FreeWheeling.Domain/DataContexts/CycleMigrations/201312030601165_CycleEntityUpdate.cs
@@ -0,0 +1,16 @@
+namespace FreeWheeling.Domain.DataContexts.CycleMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CycleEntityUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ FreeWheeling.Domain/DataContexts/CycleMigrations/201312030636104_AddFKeys.cs
@@ -0,0 +1,16 @@
+namespace FreeWheeling.Domain.DataContexts.CycleMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFKeys : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
