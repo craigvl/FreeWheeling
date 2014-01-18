617726806c21906b58e3b88bbdbb418c27d652a3
Craig Waite <undead_craig@hotmail.com>
2014-01-18 22:27:23 +1000

    Make sure map shown when all actions complete.

--- FreeWheeling.UI/Controllers/RideController.cs
+++ FreeWheeling.UI/Controllers/RideController.cs
@@ -313,7 +313,13 @@ namespace FreeWheeling.UI.Controllers
             repository.Save();
 
             Ad_HocRide Ah = repository.GetAdHocRideByID(RideComment.adhocrideid);
-            AdHocViewModel adHocViewModel = new AdHocViewModel { Ride = Ah, RideDate = Ah.RideDate, RideTime = Ah.RideTime, };
+            AdHocViewModel adHocViewModel = new AdHocViewModel
+            {
+                Ride = Ah,
+                RideDate = Ah.RideDate,
+                RideTime = Ah.RideTime,
+                MapUrl = string.Concat("<iframe id=mapmyfitness_route src=https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=http://veloroutes.org/k/%3Fr%3D", Ah.MapUrl, "&output=embed height=300px width=300px frameborder=0></iframe>")
+            };
 
             TimeZoneInfo TZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
             adHocViewModel.Riders = repository.GetRidersForAdHocRide(RideComment.adhocrideid,TZone);
@@ -440,7 +446,13 @@ namespace FreeWheeling.UI.Controllers
             repository.Save();
 
             Ad_HocRide Ah = repository.GetAdHocRideByID(adhocrideid);
-            AdHocViewModel adHocViewModel = new AdHocViewModel { Ride = Ah, RideDate = Ah.RideDate, RideTime = Ah.RideTime, };
+            AdHocViewModel adHocViewModel = new AdHocViewModel
+            {
+                Ride = Ah,
+                RideDate = Ah.RideDate,
+                RideTime = Ah.RideTime,
+                MapUrl = string.Concat("<iframe id=mapmyfitness_route src=https://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=http://veloroutes.org/k/%3Fr%3D", Ah.MapUrl, "&output=embed height=300px width=300px frameborder=0></iframe>")
+            };
 
             TimeZoneInfo TZone = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
             adHocViewModel.Riders = repository.GetRidersForAdHocRide(adhocrideid, TZone);
