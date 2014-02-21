db5f62e9c41a3ef968fb95c2a5714d561fa44f0b
Craig Waite <undead_craig@hotmail.com>
2014-02-21 17:19:18 +1000

    Update to add email to register form.

--- FreeWheeling.UI/Controllers/AccountController.cs
+++ FreeWheeling.UI/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace FreeWheeling.UI.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.UserName };
+                var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
