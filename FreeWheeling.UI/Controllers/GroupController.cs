742ff83433556e80f30265e8e031bbcd877564ee
Craig Waite <craigvl@tpg.com.au>
2014-10-07 17:01:15 +1000

    Add geocode to location create.

--- FreeWheeling.UI/Controllers/GroupController.cs
+++ FreeWheeling.UI/Controllers/GroupController.cs
@@ -430,7 +430,9 @@ namespace FreeWheeling.UI.Controllers
                 CreatedTimeStamp = LocalNow,
                 MapUrl = _GroupCreateModel.MapUrl,
                 IsPrivate = _GroupCreateModel.IsPrivate,
-                CreatedByName = _GroupCreateModel.CreatorName
+                CreatedByName = _GroupCreateModel.CreatorName,
+                Lat = _GroupCreateModel.lat,
+                Lng = _GroupCreateModel.lng
             };
 
             repository.AddGroup(NewGroup);
